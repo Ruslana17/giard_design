8979386e9c226af279a285ac976738559fb6c84d
Rusłana Fankowska <55513221+Ruslana17@users.noreply.github.com>
2023-08-29 12:19:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ oferta.addEventListener('click', () => {
 menuOferta.classList.toggle('flex')
 })
 
-menuOferta.addEventListener('click', (event)=>{
+/*menuOferta.addEventListener('click', (event)=>{
   if(event.target.classList.contains('menuOferta')){
         menuOferta.classList.toggle('flex_none') 
     }
-})
+})*/
